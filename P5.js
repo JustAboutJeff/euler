2a7fcee831f82f20bc81e920be6c34d4ae357388
JEFF <justaboutjeff@gmail.com>
2013-03-05 18:42:55 -0500

    identifies divisors in set and initial smallest solution before loop

--- P5.js
+++ P5.js
@@ -9,23 +9,35 @@ http://projecteuler.net/problem=5
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 */
 
+
 function getSolution (low,high) {
 
-	var small = high * low;
+	var divisors = [];
+	var i = 0;
 
 	while(low <= high) {
-		while(small % low != 0){
-			small++;
-		}
+		divisors.push(low);
 		low++;
 	}
-	
-	return small;
 
+	console.log('Divisors are: ' + divisors);
+
+	var small = divisors[divisors.length - 1]
+
+	console.log('Small before loop is: ' + small);
+
+	while (i <= divisors.length) {
+		if((small % divisors[i]) === 0) {
+			i++;
+		} else {
+			small++;
+			i = 0;
+		}
+	}
+
+	console.log('Small after loop is: ' + small);
+	return small;
 }
 
-getSolution(1,20);
 
-
-
-
+getSolution(1,4);
