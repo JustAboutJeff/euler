6f907583126046e27433f5864b8c68e73d005cfc
JEFF <justaboutjeff@gmail.com>
2013-03-07 11:54:46 -0500

    Solves P6

---
+++ P6.js
@@ -0,0 +1,51 @@
+/*
+Sum square difference
+Problem 6
+
+http://projecteuler.net/problem=6
+
+The sum of the squares of the first ten natural numbers is,
+1^2 + 2^2 + ... + 10^2 = 385
+
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)^2 = 55^2 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+*/
+
+function SumSquareDiff (magicNumber) {
+	var naturals = [];
+	var mySquare = 0;
+	var mySum = 0;
+	this.getNaturals = function() {
+		for(var i = 1;i <= magicNumber;i++) {
+			naturals.push(i);
+		}
+	}
+	this.getSum = function() {
+		var j = naturals.length;
+		while(j--) {
+			mySum += Math.pow(naturals[j],2);
+		}
+	}
+	this.getSquare = function() {
+		var k = naturals.length;
+		while(k--) {
+			mySquare += naturals[k];
+		}
+		mySquare = Math.pow(mySquare,2);
+	}
+	this.solve = function() {
+		var myDiff = (mySquare - mySum);
+		return myDiff;
+	}
+}
+
+
+var mySolution = new SumSquareDiff(100);
+mySolution.getNaturals();
+mySolution.getSum();
+mySolution.getSquare();
+mySolution.solve();
