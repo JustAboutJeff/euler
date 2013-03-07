76739dba2ade1aabe4a158f603e9b0db3bd05780
JEFF <justaboutjeff@gmail.com>
2013-03-07 12:13:24 -0500

    Refactors solution with fewer loop calls

--- P6.js
+++ P6.js
@@ -16,36 +16,29 @@ Find the difference between the sum of the squares of the first one hundred natu
 */
 
 function SumSquareDiff (magicNumber) {
+
 	var naturals = [];
 	var mySquare = 0;
 	var mySum = 0;
+
 	this.getNaturals = function() {
 		var i = magicNumber;
 		while(i--) {
 			naturals.push(i);
 		}
 	}
-	this.getSum = function() {
+
+	this.solve = function() {
 		var j = magicNumber;
 		while(j--) {
 			mySum += Math.pow(naturals[j],2);
-		}
-	}
-	this.getSquare = function() {
-		var k = magicNumber;
-		while(k--) {
-			mySquare += naturals[k];
+			mySquare += naturals[j];
 		}
 		mySquare = Math.pow(mySquare,2);
-	}
-	this.solve = function() {
 		return (mySquare - mySum);
 	}
 }
 
-
 var mySolution = new SumSquareDiff(100);
 mySolution.getNaturals();
-mySolution.getSum();
-mySolution.getSquare();
 mySolution.solve();
